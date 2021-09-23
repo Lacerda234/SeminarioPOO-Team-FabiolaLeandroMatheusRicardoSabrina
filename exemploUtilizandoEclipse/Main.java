0843b2363d12bf08eeeaf64266ccceae5271693e
RicardoOliveira89 <58044008+RicardoOliveira89@users.noreply.github.com>
2021-09-23 00:01:56 -0300

    Exemplo Utilizando Eclipse

---
+++ exemploUtilizandoEclipse/Main.java
@@ -0,0 +1,23 @@
+package exemploUtilizandoEclipse;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Mensagem demonstrativa.
+
+        System.out.println("\nSeminário da disciplina de Programação Orientada a Objetos. "
+                + "\nTema - Comparativo de IDEs Java. "
+                + "\nGrupo 2 - Fabiola Diniz, Leandro Felix, Matheus Valença,  Ricardo Oliveira e Sabrina Salviano.");
+
+        // Soma de dois números inteiros.
+
+        int number1 = 5;
+        int number2 = 7;
+
+        int soma = number1 + number2;
+
+        System.out.println("\nO resultado é " + soma + ".");
+    }
+
+}
