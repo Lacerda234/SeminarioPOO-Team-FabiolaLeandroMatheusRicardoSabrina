947e9a32c1f96165117074b11527c39ad7e8a3a8
Fabíola Diniz <fabioladiniz36@hotmail.com>
2021-09-23 13:02:50 +0000

    Exemplo de codigo para Bluej

---
+++ ExemploUtilizandoBluej/Olapessoal.java
@@ -0,0 +1,13 @@
+
+/**
+ * Programa de demonstração da IDE Bluej
+ * 
+ * @author Fabíola Diniz
+ * @version  1.0 20-09-21
+ */
+public class OlaPessoal {
+  public static void main(String args[]) {
+    System.out.println("Ola pessoal!");
+    System.out.println("Vamos da uma Olhada no Bluej?");
+  }
+}
