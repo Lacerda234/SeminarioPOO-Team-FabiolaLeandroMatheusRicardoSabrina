4d6a8a394fdc0436730c88cd382cc57b948bcd88
Sabrina Salviano <sabrina.salviano.0934@gmail.com>
2021-09-22 22:42:10 -0300

    Exemplo utilizando o IntelliJ IDEA.

---
+++ ExemploUsandoIntelliJ/testando.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class testando {
+    public static void main(String []args){
+
+        float nota1=8, nota2=7, nota3=10, soma, media;
+
+        soma= nota1 + nota2 + nota3;
+        media = soma / 3;
+
+        JOptionPane.showMessageDialog(null, " A media eh: "+media);
+
+    }
+}
